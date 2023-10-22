ff50c939e7e6cfcadbf411232c6fd112ed450d82
git-alexandre-reis <alexandre_siqueira_reis@hotmail.com>
2023-10-22 15:52:56 -0300

    Alterações carrinho

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -6,8 +6,14 @@ import formatCurrency from '../../util/formatCurrency';
 import ItemCart from '../itemCart/ItemCart';
 import { Cartcontext } from '../../context/Context';
 
+import { MdClose } from 'react-icons/md';
+
 const Cart = () => {
-  const {products, isCartVisible} = useContext(Cartcontext);
+  const {products, isCartVisible, setIsCartVisible} = useContext(Cartcontext);
+
+  const close = ()=>{
+    setIsCartVisible(!isCartVisible);
+  };
   
   const total = products.reduce((total, item) => {
     return total + item.price * item.qty;
@@ -15,38 +21,45 @@ const Cart = () => {
 
   return (
     <>
-      { products.length === 0?
-        <div className={`carrinho-vazio ${isCartVisible ? 'active' : ''}`} >
-          <h2>Carrinho Vazio</h2>
-        </div>:
-        <div className={`carrinho ${isCartVisible ? 'active' : ''}`} >
-          <div className="itens-carrinho">
-            {products.map(produto => 
-              <ItemCart
-                key={produto.id} 
-                data={
-                  {
-                    id:produto.id,
-                    thumbnail: produto.thumbnail.replace(/\w\.jpg/gi, 'W.jpg'),
-                    title: produto.title,
-                    price: produto.price,
-                    qty: produto.qty,
-                    amount: produto.amount * produto.qty
+      <div className={`cart_popup_modal ${isCartVisible ? 'active' : ''}`}>
+        <div className="overlay" onClick={close}></div>
+        {products.length === 0?
+          <div className={`carrinho-vazio ${isCartVisible ? 'active' : ''}`} >
+            <h2>Carrinho Vazio</h2>
+          </div>:
+          <div className="carrinho" >
+            <div className="cartHeader">
+              <h2>Shopping Cart</h2>
+              <MdClose onClick={close}/>
+            </div>
+            <div className="itens-carrinho">
+              {products.map(produto => 
+                <ItemCart
+                  key={produto.id} 
+                  data={
+                    {
+                      id:produto.id,
+                      thumbnail: produto.thumbnail.replace(/\w\.jpg/gi, 'W.jpg'),
+                      title: produto.title,
+                      price: produto.price,
+                      qty: produto.qty,
+                      amount: produto.amount * produto.qty
+                    }
                   }
-                }
-              />
-            )}
-          </div>
+                />
+              )}
+            </div>
         
-          <div className="total-carrinho">
-            <div className="total">
-              <h4>Total: <span>{formatCurrency(total, 'BRL')}</span> + frete</h4>
+            <div className="total-carrinho">
+              <div className="total">
+                <h4>Total: <span>{formatCurrency(total, 'BRL')}</span> + frete</h4>
+              </div>
+              <div className="finalizar">
+                <a href="#">Finalizar</a>
+              </div>
             </div>
-            <div className="finalizar">
-              <a href="#">Finalizar</a>
-            </div>
-          </div>
-        </div>}
+          </div>}
+      </div>
     </>
   );
 };
